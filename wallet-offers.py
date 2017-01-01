feb9b8c642d0d06ad2196e2466a03b7f119fc47c
Russ Katz <russkatz@rkatz-rmbp15.local>
2016-12-31 19:51:48 -0600

    initial commit

---
+++ wallet-offers.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+import time
+import sys
+
+from cassandra.cluster import Cluster
+from cassandra.auth import PlainTextAuthProvider
+
+#Configuration
+contactpoints = ['10.0.0.252']
+auth_provider = PlainTextAuthProvider (username='russ', password='DevryPOC')
+keyspace = "wallet"
+
+print "Connecting to cluster"
+
+cluster = Cluster( contact_points=contactpoints,
+                   auth_provider=auth_provider )
+
+session = cluster.connect()
+
+query1 = "CREATE KEYSPACE IF NOT EXISTS %s WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1'}  AND durable_writes = true;" % (keyspace)
+query2 = "CREATE TABLE IF NOT EXISTS %s.wallet_by_id ( id timeuuid, rev timeuuid, createdtime text, deleted boolean, email text, firstname text, hashedemail text, lastname text, loyalityid text, profileid text, updatedtime text, PRIMARY KEY (id, rev));" % (keyspace)
+query3 = "CREATE TABLE IF NOT EXISTS %s.wallet_offers ( ownedbywallet text, id text, createdtime text, deleted text, effectivebegindate text, effectiveenddate text, itemid text, otype text, promocode text, shareable text, supcused text, updatedtime text, PRIMARY KEY (ownedbywallet, id));" % (keyspace)
+query4 = "CREATE TABLE IF NOT EXISTS %s.wallet_newitems ( id text PRIMARY KEY, createdtime text, deleted boolean, effectivebegindate text, effectiveenddate text, itemid text, promocode text, shareable text, supcused text, type text, updatedtime text);" % (keyspace)
+query5 = "INSERT INTO %s.wallet_newitems (id , effectiveenddate, createdtime, deleted, effectivebegindate, itemid, promocode, shareable, supcused, type, updatedtime) VALUES ( '1','12/30/2017','12/25/2016', false, '12/31/2016', '1', 'NEW ITEM', 'no', 'something', 'coupon', '12/27/2016');" % (keyspace)
+
+print "Creating keyspace and tables"
+session.execute(query1)
+session.execute(query2)
+session.execute(query3)
+session.execute(query4)
+print "Creating new offer"
+session.execute(query5)
+
+print "Creating wallets (CTRL+C to stop)"
+id = 1
+count = 1
+loop = 1
+while loop == 1:
+   query = "INSERT INTO %s.wallet_by_id (id, firstName, lastName, hashedEmail, profileId, loyalityId, createdTime, updatedTime, deleted, rev) VALUES (now(), 'Russ2', 'Katz2', 'russ@email.com', 'p12346', 'l12346', '12/30/2016', '12/30/2016', false, now());" % (keyspace)
+   session.execute(query)
+   id = id + 1
+   count = count + 1
+   if count == 10000:
+      print "Wallets created: ", id
+      count = 0
+
+cluster.shutdown()
+sys.exit(0)
---
+++ wallet_new_offer.scala
@@ -0,0 +1,7 @@
+case class WalletItem(ownedbywallet: String, id: String, effectiveenddate: String, createdtime: String, deleted: String, effectivebegindate: String, itemid: String, promocode: String, shareable: String, supcused: String, otype: String, updatedtime: String)
+
+var ids = sc.cassandraTable("wallet","wallet_by_id").select("id").as((i:String) => (i)).collect()
+
+var item = sc.cassandraTable("wallet","wallet_newitems").where("id = '1'").as((id:String,ed:String,cd:String,d:String,ebd:String,ii:String,p:String,s:String,sup:String,t:String,ut:String) => (id,ed,cd,d,ebd,ii,p,s,sup,t,ut)).collect()
+
+for (x <- ids) yield item.map{ case (a,b,c,d,e,f,g,h,i,j,k) => sc.parallelize(Seq((x,a,b,c,d,e,f,g,h,i,j,k))).saveToCassandra("wallet","wallet_offers")}
